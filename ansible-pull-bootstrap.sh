03620aa442eb5ebb4efaf21059df51d33a0b103e
Thomas Delaet <thomas@delaet.org>
2022-10-13 12:28:46 +0200

    update

--- ansible-pull-bootstrap.sh
+++ ansible-pull-bootstrap.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-touch /root/test
+KEY=""
+
+#just run to enable ansible-pull timer when booted
+
+#ansible-pull \
+#    -U "git@github.com:thomasdelaet/ansible.git" \
+#    -i "$(hostname --short)" \
+#    --private-key ${KEY} \
+#    --accept-host-key \
+#    --verbose \
+#    --tags ansible
+
+echo "$(hostname --short)" > /tmp/test
