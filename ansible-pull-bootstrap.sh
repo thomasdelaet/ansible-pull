41eac15a245c507dd978f1d91e18e0fbe08837b6
Thomas Delaet <thomas@delaet.org>
2022-10-15 02:38:07 +0200

    update

--- ansible-pull-bootstrap.sh
+++ ansible-pull-bootstrap.sh
@@ -3,3 +3,5 @@
 mv /root/ansible-pull-cron /etc/cron.d/ansible-pull
 chmod o-rwx /root/.ssh
 chmod o-rwx /root/.ssh/deployment_key
+mkdir -p /etc/ansible
+echo "$(hostname --short) ansible_connection=local" > /etc/ansible/hosts
