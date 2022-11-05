753bf08c889141ca78f4e6f7b03122f3407be1f1
Thomas Delaet <thomas@delaet.org>
2022-11-05 23:27:55 +0100

    update

--- ansible-pull-bootstrap.sh
+++ ansible-pull-bootstrap.sh
@@ -11,6 +11,11 @@ chmod -R o-rwx,g-rwx /root
 chmod 644 /root/.ssh/config
 mkdir -p /etc/ansible
 echo "${inventory_hostname} ansible_connection=local" > /etc/ansible/hosts
-git clone git@github.com:thomasdelaet/ansible.git /root/ansible
+if [ ! -d "/root/ansible" ]
+then
+    git clone git@github.com:thomasdelaet/ansible.git /root/ansible
+else
+    (cd /root/ansible; git pull origin)
+fi
 chown -R root:root /root
 chmod -R o-rwx,g-rwx /root
